ec7256ed5d418ca424d96f1d00c921c3412be7af
asep Hidayat <hidayatasep2901@gmail.com>
2016-12-22 10:55:51 +0700

    iniate commit

---
+++ app/src/main/java/com/hidayatasep/makersinstitute/belajarcustomlist/Word.java
@@ -0,0 +1,31 @@
+package com.hidayatasep.makersinstitute.belajarcustomlist;
+
+public class Word {
+
+    String englishWord;
+    String indonesianWord;
+
+    public Word(String englishWord, String indonesianWord) {
+        this.englishWord = englishWord;
+        this.indonesianWord = indonesianWord;
+    }
+
+    public String getEnglishWord() {
+        return englishWord;
+    }
+
+    public void setEnglishWord(String englishWord) {
+        this.englishWord = englishWord;
+    }
+
+    public String getIndonesianWord() {
+        return indonesianWord;
+    }
+
+    public void setIndonesianWord(String indonesianWord) {
+        this.indonesianWord = indonesianWord;
+    }
+}
+
+
+
